d04e58e55c7cae5d4a328e4bf4d62e77ad372b86
Atipat D <95071528+Atipat-CMU@users.noreply.github.com>
2024-03-30 23:08:22 +0700

    final submition

--- tests/ca1/ca1.cpp
+++ tests/ca1/ca1.cpp
@@ -8,7 +8,7 @@ using namespace dip;
 int main(int argc, char const *argv[])
 {
     Image image;
-    image = imread("../src/ca1/random_shape.pgm");
+    image = imread("../src/ca1/scaled_shapes.pgm");
     // image.printHistogram();
 
     BlobColoring blobs(image, 0, 230);
@@ -16,11 +16,11 @@ int main(int argc, char const *argv[])
 
     cout << blobs.nums() << endl;
 
-    // for(int i = 1; i <= blobs.nums(); i++){
-    //     Mask obj = blobs.getLabelMask().toBinary(i);
-    //     Moment obj_moment(obj);
-    //     cout << "invariant of Object " << i << ": " << obj_moment.getInvariant() << endl;
-    // }
+    for(int i = 1; i <= blobs.nums(); i++){
+        Mask obj = blobs.getLabelMask().toBinary(i);
+        Moment obj_moment(obj);
+        cout << "invariant of Object " << i << ": " << obj_moment.getInvariant() << endl;
+    }
 
     Image cropedImage = image.crop(frames[10]);
     imwrite(cropedImage, "../out/ca1/crop.pgm");
